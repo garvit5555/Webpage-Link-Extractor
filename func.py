eeadf3e70a00c505faf9d478453b6a01a30eb480
Garvit <153619593+garvit5555@users.noreply.github.com>
2025-01-18 22:07:35 +0530

    Update func.py

--- func.py
+++ func.py
@@ -1,29 +1,42 @@
 import streamlit as st
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse
+
 # extract links to fetch and parse links from a webpage
 def extract_links(url):
-    response = requests.get(url)
+    try:
+        response = requests.get(url)
 
-    # if extracted
-    if response.status_code == 200:
+        # if extracted
+        if response.status_code == 200:
 
-        soup = BeautifulSoup(response.text, 'html.parser')
+            soup = BeautifulSoup(response.text, 'html.parser')
 
-        links = soup.find_all('a', href=True)
+            links = soup.find_all('a', href=True)
 
-        # Filter out unwanted/dummy links
-        valid_links = [link for link in links if not link['href'].startswith(('javascript:', '#'))]
+            # Filter out unwanted/dummy links
+            valid_links = [link for link in links if not link['href'].startswith(('javascript:', '#'))]
 
-        # total number of valid links
-        st.write(f"### Total number of valid links found: {len(valid_links)}")
+            #firstly extract the domain of the entered URL
+            base_domain = urlparse(url).netloc
 
-        # Deploy all valid URLs using streamlit
-        for i, link in enumerate(valid_links, 1):
-            href = link['href']
-            st.write(f"**{i}. Original link:** {href}")
-            st.write(f"({urljoin(url, href)})")
-            st.markdown("---")
-    else:
-        st.error(f"Failed to retrieve the webpage. Status code: {response.status_code}") ## if not extracted
+            # we will be taking the links that belongs only to the domain of entered URL
+            same_domain_links = [link for link in valid_links if urlparse(urljoin(url, link['href'])).netloc == base_domain]
+
+            # total number of valid links
+            st.write(f"### Total number of valid links found: {len(same_domain_links)}")
+
+            # Display all valid URLs using Streamlit
+            for i, link in enumerate(same_domain_links, 1):
+                href = link['href']
+                full_url = urljoin(url, href)
+                st.write(f"**{i}. Original link:** {href}")
+                st.write(f"({full_url})")
+                st.markdown("---")
+        else:
+            st.error(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
+
+# end
